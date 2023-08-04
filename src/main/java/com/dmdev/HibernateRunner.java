726083f0a1e5faba1bc97c47ddadf354ab4bf309
AlexandrLavrinenko <alexandr.lavrinenko@gmail.com>
2023-08-04 20:45:39 +0300

    N + 1 problem: @EntityGraph

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -7,9 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.annotations.QueryHints;
+import org.hibernate.graph.GraphSemantic;
+import org.hibernate.graph.RootGraph;
+import org.hibernate.query.Query;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class HibernateRunner {
@@ -18,13 +23,22 @@ public class HibernateRunner {
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
             session.beginTransaction();
-            session.enableFetchProfile("withCompanyAndPayments");
 
-            User user = session.get(User.class, 1L);
+//            User user = session.get(User.class, 1L);    // get не подхоит, не обладает нужнім API
+            Map<String, Object> properties = Map.of(
+                    GraphSemantic.LOAD.getJpaHintName(), session.getEntityGraph("WithCompanyAndChat")
+            );
+            User user = session.find(User.class, 1L, properties);
             System.out.println(user.getCompany().getName());
+            System.out.println(user.getUserChats().size());
 
-//            users.forEach(user -> System.out.println(user.getPayments().size()));
-//            users.forEach(user -> System.out.println(user.getCompany().getName()));
+
+            List<User> users = session.createQuery("select u from User u " +
+                                                  "where 1 = 1", User.class)
+                    .setHint(GraphSemantic.LOAD.getJpaHintName(), session.getEntityGraph("WithCompanyAndChat"))
+                    .list();
+            users.forEach(it -> System.out.println(it.getCompany().getName()));
+            users.forEach(it -> System.out.println(it.getUserChats().size()));
 
 
             session.getTransaction().commit();
